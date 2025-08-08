69e5fa87c452ca4e4f30c1c730f795adc4ce75fb
Vaibhav Kumbhar <vnkdj5@gmail.com>
2025-08-08 22:58:48 +0530

    Fix routing for GitHub Pages: switch to HashRouter and add detailed build logging

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "@/components/ThemeProvider";
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <ThemeProvider>
           <Routes>
             <Route element={<Layout />}>
@@ -25,7 +25,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
